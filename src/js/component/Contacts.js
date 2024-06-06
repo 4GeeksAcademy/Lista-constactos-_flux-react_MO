1d28b6c6fce9d406e2b4433b5185a0a2e86d49d5
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-06-06 21:16:35 +0000

    pendiente hoja datos contactos

---
+++ src/js/component/Contacts.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export const ListContact = () => {
+  return (
+    <div className=" bg-success-subtle container mt-2 " >
+      <h1 className="text-center mb-4">Add a new contact</h1>
+    <div> 
+        <div class="card mb-3" style="max-width: 540px;" >
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="..." />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                  <span>  <button type="submit" className="btn text-center">edit</button></span>
+                  <span>  <button type="submit" className="btn text-center">delete</button></span> </div>
+              </div>
+            </div>
+          </div>
+          )</div>    
+          </div>    
+          )};
--- src/js/component/ListContact.js
+++ src/js/component/ListContact.js
@@ -1,35 +1,63 @@
-
 import React, { Component } from "react";
 
 export const ListContact = () => {
-  return(
-<div className="container mt-5"> 
-<h1 className="text-center mb-4">Add a new contact</h1>
-<htmlForm>
-  <div classNameName="mb-3">
-  <h5>Full Name</h5>
-    <label htmlFor="exampleInputFullName" className="Form-label"></label>
-    <input type="fullName" className="Form-control w-50" id="exampleInputFullName" aria-describedby="emailHelp" placeholder="Full name" />
-      </div>
-  <div className="mb-3">
-  <h5>Email</h5>
-    <label htmlFor="exampleInputEmail" className="Form-label"></label>
-    <input type="Email" className="Form-control justify-content-center w-50" id="exampleInputPhone" placeholder="Email"/>
-  </div>
-  <div classNameName="text-center mb-3">
-  <h5>Phone</h5>
-    <label htmlFor="exampleInputPhone" className="Form-label"></label>
-    <input type="Phone" className="Form-control justify-content-center w-50" id="exampleInputPhone" aria-describedby="emailHelp" placeholder="Phone" />
-      </div>
-  <div className="mb-3">
-  <h5>Adress</h5>
-    <label htmlFor="exampleInputAddress" className="Form-label"></label>
-    <input type="Address" className="Form-control justify-content-center w-50" id="exampleInputAddress" placeholder= "adress" />
-  </div>
-  
-  <button type="submit" className=" d-flex justify-content-center btn btn-primary btn-lg w-50">Save</button>
-  <button type="submit" className="btn text-center">or get to contacts</button>
-</htmlForm>
-</div>
-  )
+  return (
+    <div className=" bg-success-subtle container mt-2 ">
+      <h1 className="text-center mb-4">Add a new contact</h1>
+      <htmlForm>
+        <div classNameName="mb-3">
+          <h5>Full Name</h5>
+          <label htmlFor="exampleInputFullName" className="Form-label "></label>
+          <input
+            type="fullName"
+            className="Form-control w-50"
+            id="exampleInputFullName"
+            aria-describedby="emailHelp"
+            placeholder="Full name"
+          />
+        </div>
+        <div className="mb-3">
+          <h5>Email</h5>
+          <label htmlFor="exampleInputEmail" className="Form-label"></label>
+          <input
+            type="Email"
+            className="Form-control justify-content-center w-50"
+            id="exampleInputPhone"
+            placeholder="Email"
+          />
+        </div>
+        <div classNameName="text-center mb-3">
+          <h5>Phone</h5>
+          <label htmlFor="exampleInputPhone" className="Form-label"></label>
+          <input
+            type="Phone"
+            className="Form-control justify-content-center w-50"
+            id="exampleInputPhone"
+            aria-describedby="emailHelp"
+            placeholder="Phone"
+          />
+        </div>
+        <div className="mb-3">
+          <h5>Adress</h5>
+          <label htmlFor="exampleInputAddress" className="Form-label"></label>
+          <input
+            type="Address"
+            className="Form-control justify-content-center w-50"
+            id="exampleInputAddress"
+            placeholder="adress"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className=" d-flex justify-content-center btn btn-primary btn-lg w-50"
+        >
+          Save
+        </button>
+        <button type="submit" className="btn text-center">
+          or get to contacts
+        </button>
+      </htmlForm>
+    </div>
+  );
 };
